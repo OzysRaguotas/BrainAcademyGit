4e2cae3152f409c6b355992efcbf8308e0acdf79
OzysRaguotas <yeriomin.slava@gmail.com>
2016-07-15 20:28:35 +0300

    A lot of changes

---
+++ Customer.cs
@@ -0,0 +1,12 @@
+using System;
+using Collections.Generic;
+
+namespace BrainAcademyGIT
+{
+public class Customer
+{
+List<Products> products { get; set; }
+        public Customer(List<Products> products)
+        { get;  set; }
+}
+}
