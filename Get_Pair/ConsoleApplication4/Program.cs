de7232570f9001a0085f7cd23bde16bedd333fc3
OzysRaguotas <yeriomin.slava@gmail.com>
2016-07-27 19:50:22 +0300

    Get pair numbers

---
+++ Get_Pair/ConsoleApplication4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] MyArr = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                MyArr[i] = i + 1;
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(MyArr[i]);
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                if (MyArr[i] % 2 == 0)
+                {
+                    Console.WriteLine(MyArr[i]);
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
