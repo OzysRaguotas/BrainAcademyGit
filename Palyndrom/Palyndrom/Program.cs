736e784fce6e793651d38c3b81bdb1da5582cd1b
OzysRaguotas <yeriomin.slava@gmail.com>
2016-07-28 18:02:28 +0300

    Labs for fun

---
+++ Palyndrom/Palyndrom/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palyndrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get a word from console
+            Console.WriteLine("Enter word");
+            string word = Console.ReadLine();
+            // Convert it into char array
+            char[] Arr = word.ToCharArray();
+            
+            bool check = false;
+            for (int i = 0; i < Arr.Length / 2; i++)
+            {
+                if (Arr[Arr.Length - 1 - i] == Arr[i])
+                {
+                    check = true;
+                }
+                else
+                {
+                    check = false;
+                    Console.WriteLine("-");
+                    Console.WriteLine("No, entered word is not a palyndrom");
+                }
+            }
+            if (check == true)
+                Console.WriteLine("Yes, entered word is palyndrom");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Spacer/Spacer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spacer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = "I'm a black rainbow, I'm an ape of God, I got a face that's made for violence upon";
+            char char1 = ' ';
+            for (int i = 0; i < string1.Length; i++)
+            {
+                if (string1[i] != char1)
+                {
+                    string string2 = Convert.ToString(string1[i]);
+                    Console.Write("{0}", string2);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
