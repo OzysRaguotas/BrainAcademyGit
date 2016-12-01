ff6b24532d592451101f0592e5bfbbf0a6e1ec01
OzysRaguotas <yeriomin.slava@gmail.com>
2016-12-01 18:51:59 +0200

    Test for YM

---
+++ WebApplication2/WebApplication2/DAL/EmployeeContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication2.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace WebApplication2.DAL
+{
+    public class EmployeeContext: DbContext
+    {
+        public EmployeeContext() : base("EmployeeContext")
+        {
+        }
+
+        public DbSet<EmployeeModel> Employes { get; set; }
+       
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/DAL/EmployeeInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using WebApplication2.Models;
+
+namespace WebApplication2.DAL
+{
+    public class EmployeeInitializer: System.Data.Entity.DropCreateDatabaseIfModelChanges<EmployeeContext>
+    {
+        protected override void Seed(EmployeeContext context) {
+
+            var employers = new List<EmployeeModel> {
+            new EmployeeModel()
+            {
+                Id = 1001,
+                Name = "Ivan",
+                Department = "Sales"
+            },
+            new EmployeeModel()
+            {
+                Id = 1002,
+                Name = "Vasily",
+                Department = "Service center"
+            },
+            new EmployeeModel()
+            {
+                Id = 1003,
+                Name = "Masha",
+                Department = "Finance department"
+            }};
+            employers.ForEach(s => context.Employes.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
